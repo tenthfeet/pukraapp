d3db31eb70ec8b9e98eb4c00fb6e7d5642bc1c20
srishakthim <srishakthimoorthy235@gmail.com>
2026-01-23 15:47:18 +0530

    mail and app logo fixed

--- src/config/API_URLS.tsx
+++ src/config/API_URLS.tsx
@@ -2,7 +2,7 @@ import { API_BASE_URL } from '@env';
 console.log('Base URL', API_BASE_URL);
 
 const API_URLS = {
-  BASE_URL: API_BASE_URL || 'https://c903ffa839ce.ngrok-free.appp',
+  BASE_URL: API_BASE_URL || ' https://5276afa69cfe.ngrok-free.app ',
 
   // Public
   DOCTORS: '/doctors',
